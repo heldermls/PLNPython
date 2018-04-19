4b2bdf6dfc0e6c75f2c10081ac74da79aedba8c2
helder silva <heldermls@hotmail.com>
2018-04-19 18:21:55 -0300

    agora sim

---
+++ py/geradorXMLPatrocinadores.py
@@ -0,0 +1,38 @@
+# from xml.etree.ElementTree import Element,ElementTree
+import re
+import io
+from py import util_strings as util
+import unicodedata
+import xml.dom.minidom as minidom
+import xml.etree.ElementTree as et
+
+root = et.Element('patrocinadores')
+tree = et.ElementTree(root)
+with open('../txt/Patrocinadores.txt', encoding="utf8") as f:
+    txt = f.read()
+    clean_text = unicodedata.normalize("NFKD", txt)
+    lines = clean_text.splitlines()
+
+
+for line in lines:
+
+    if not line or bool(re.match(r'\s\s*', line)):
+        print("linha vazia")
+    else:
+
+        patroninador = et.SubElement(root, "patrocinador")
+        patroninador.text = line
+
+
+f.close()
+#prettify xml
+
+formatedXML = minidom.parseString(et.tostring(root)).toprettyxml(indent=" ").strip()
+#print(formatedXML)
+
+#tree.write('diretorias.xml',  method='xml')
+# write the formatedXML to file.
+with io.open("../xml/Patrocinadores.xml", "w+", encoding="utf-8") as f:
+    f.write(formatedXML)
+
+
