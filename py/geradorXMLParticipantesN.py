ba69d796eb31e3d591ee0a3f458938fee419e3e4
helder silva <heldermls@hotmail.com>
2018-08-06 14:27:38 -0300

    Upgrades

---
+++ py/geradorXMLParticipantesN.py
@@ -0,0 +1,43 @@
+# from xml.etree.ElementTree import Element,ElementTree
+import re
+import io
+from py import util_strings as util
+import unicodedata
+import xml.dom.minidom as minidom
+import xml.etree.ElementTree as et
+
+root = et.Element('participantes')
+tree = et.ElementTree(root)
+with open('../txt/Participantes.txt', encoding="utf8") as f:
+    txt = f.read()
+    clean_text = unicodedata.normalize("NFKD", txt)
+    lines = clean_text.splitlines()
+
+
+for line in lines:
+
+    if not line or bool(re.match(r'\s\s*', line)):
+        print("linha vazia")
+    else:
+        line = util.explodeperiodo(line)
+        res = re.search(r'(\D|\s)+', line)
+        print(line)
+        participante = et.SubElement(root, "participante")
+        nome = et.SubElement(participante, "nome")
+        nome.text = res[0].strip()
+        txtanos = ""
+        tamanho = len(re.findall(r'\d\d\d\d', line))
+        nparticipacoes = et.SubElement(participante, "nparticipacoes")
+        nparticipacoes.text = str(tamanho)
+f.close()
+#prettify xml
+
+formatedXML = minidom.parseString(et.tostring(root)).toprettyxml(indent=" ").strip()
+#print(formatedXML)
+
+#tree.write('diretorias.xml',  method='xml')
+# write the formatedXML to file.
+with io.open("../xml/ParticipantesQ.xml", "w+", encoding="utf-8") as f:
+    f.write(formatedXML)
+
+
